42ec914c4a70c685fd3181c4cbc8b77df457b963
Laurent Doguin <laurent.doguin@gmail.com>
2017-03-23 19:48:47 +0100

    add server

--- index.js
+++ index.js
@@ -1,24 +1,46 @@
-//Example POST method invocation 
-var Client = require('node-rest-client').Client;
- 
-var client = new Client();
-var appID = process.env['APP_ID'];
-// set content-type header and data as json in args parameter 
-var args = {
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const Client = require('node-rest-client').Client
 
+const client = new Client()
+const app = express()
+const port = 3000
+// The id of your deployed application is accessible
+// as an envrionment variable
+const appID = process.env['APP_ID']
+
+// Change the name and email so we know who you are!
+const args = {
     data: {
         "firstname":"Alain",
     	"lastname":"Chabat",
     	"email":"regis@con",
-    	"appID":appID
+    	"id":appID
     },
     headers: { "Content-Type": "application/json" }
 };
 
-client.post("http://onboarder-breizhcamp.cleverapps.io/play", args, function (data, response) {
-    // parsed response body as js object 
-    console.log(data);
-    // raw response 
-    console.log(response);
-});
- 
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  client.post("http://onboarder-breizhcamp.cleverapps.io/play", args, function (data, response) {
+    console.log(response.statusCode);
+  });
+  response.render('home', {
+    appID: appID
+  })
+})
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
+
